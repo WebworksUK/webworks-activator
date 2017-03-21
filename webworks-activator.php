356a16d27210840390e9a65cb3f5fb841a835ecf
WebworksJoe <joe@webworks.london>
2017-03-21 13:41:21 +0000

    Add files via upload

---
+++ webworks-activator.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin Name:  Webworks required plugins activator
+ * Plugin URI:   https://webworks.london
+ * Description:  Activates iThemes Security, Litespeed Cache and Webworks Functions. This is for the benefit of your site, our infrastructure and our other users. All sites hosted on CloudWP must run these plugins.
+ * Version:      1.0
+ * Author:       Webworks UK Ltd
+ * Author URI:   https://webworks.london
+ * License:      GPLv2 or later
+ */
+
+function run_activate_plugin( $plugin ) {
+    $current = get_option( 'active_plugins' );
+    $plugin = plugin_basename( trim( $plugin ) );
+
+    if ( !in_array( $plugin, $current ) ) {
+        $current[] = $plugin;
+        sort( $current );
+        do_action( 'activate_plugin', trim( $plugin ) );
+        update_option( 'active_plugins', $current );
+        do_action( 'activate_' . trim( $plugin ) );
+        do_action( 'activated_plugin', trim( $plugin) );
+    }
+
+    return null;
+}
+run_activate_plugin( 'better-wp-security/better-wp-security.php' );
+run_activate_plugin( 'webworks-functions/webworks-functions.php' );
+run_activate_plugin( 'litespeed-cache/litespeed-cache.php' );
